afa5d2ba7b383b0f9b5235e5e776275d881c9745
Shoshana Bernstein <shoshanbernstein@gmail.com>
2026-02-11 15:24:33 -0500

    SB - Created Crud for ProductDAL

--- ShoshanaBernstein_MiriSelengut/DAL/ProductDAL.cs
+++ ShoshanaBernstein_MiriSelengut/DAL/ProductDAL.cs
@@ -14,36 +14,125 @@ namespace DAL
         static List<Product> list = new List<Product>();
 
         #region Ctor
+        //ctor that initiales list to data in the text file
         public ProductDAL()
         {
-             
+             InitializeList();
         }
         #endregion
 
         #region InitializeList
+        //initializes the list to the information in the text file
         public void InitializeList()
         {
             string[] lines = File.ReadAllLines(@"..\..\DAL\ProductList.txt");
             var index = 0;
 
+            //for each line in the file create a new object
             foreach (string line in lines)
             {
+                
                 string[] parts = line.Split(' ');
-
-                string productNumber = parts[0];
+                int productNumber = int.Parse(parts[0]);
                 string productName = parts[1];
                 float costPerUnit = float.Parse(parts[2]);
                 int amountInStock = int.Parse(parts[3]);
+                
+                Product newproduct = new Product(productNumber, productName, costPerUnit, amountInStock);
 
-               
-
+                //add the objects to the list
+                list.Add(newproduct);
             }
            
         }
         #endregion
 
         #region Create
+        //method to add a new object to list
+        public void Create(Product tmp)
+        {
+            //Check if a product with the same ProductNumber already exists
+            bool exists = list.Any(p => p.ProductNumber == tmp.ProductNumber);
+            //if it doesn't exit
+            if (!exists)
+            {
+                Product newProduct = new Product(
+                    tmp.ProductNumber,
+                    tmp.ProductName,
+                    tmp.CostPerUnit,
+                    tmp.AmountInStock
+                    );
 
+                //add it to the list
+                list.Add(newProduct);
+            }
+
+        }
+        #endregion
+
+        #region Read all the list
+        //method to return the entire list
+
+        public List<Product> Read()
+        {
+            //create a new list
+            List<Product> newList = new List<Product>();
+
+            //for each object in list, copy it
+            foreach (Product product in list)
+            {
+                Product copyProduct = new Product(
+                    product.ProductNumber,
+                    product.ProductName,
+                    product.CostPerUnit,
+                    product.AmountInStock
+                    );
+                
+                //add the copied object to newList
+                newList.Add(copyProduct);
+            }
+
+            return newList;
+
+        }
+        #endregion
+
+        #region Update
+        //method Update to change some values of a Person
+
+        public void Update(Product tmp)
+        {
+          
+            //go through the list to find the matching product number
+            for( int index = 0; index < list.Count(); index++)
+            {
+                //if the match, create a new object and replace the found object
+                if (list[index].ProductNumber == tmp.ProductNumber)
+                {
+                    Product changedProduct = new Product(tmp.ProductNumber, tmp.ProductName, tmp.CostPerUnit, tmp.AmountInStock);
+                    list[index] = changedProduct;
+                }
+            }
+
+        }
+        #endregion
+
+        #region Delete
+        // method to Delete a product from the list
+        public void Delete(Product tmp)
+        {
+            //go thru list to find product whose number matches the number of Person parameter
+            for(int index = 0; index < list.Count();index++)
+            {
+                //if the products numbers match
+                if (list[index].ProductNumber == tmp.ProductNumber)
+                {
+                    //remove the object from the list
+                    list.RemoveAt(index);
+                    break;
+                }
+            }
+        }
         #endregion
     }
 }
--- ShoshanaBernstein_MiriSelengut/DAL/Program.cs
+++ ShoshanaBernstein_MiriSelengut/DAL/Program.cs
@@ -10,6 +10,8 @@ namespace DAL
     {
         static void Main(string[] args)
         {
+            ProductDAL products = new ProductDAL();
+
         }
     }
 }
