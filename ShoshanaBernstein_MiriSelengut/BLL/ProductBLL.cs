c0a03b219694e01e1cd467bba750f0e432ab45f6
Shoshana Bernstein <shoshanbernstein@gmail.com>
2026-02-24 17:16:47 -0500

    SB - Started BLL and ProductBLL

---
+++ ShoshanaBernstein_MiriSelengut/BLL/ProductBLL.cs
@@ -0,0 +1,34 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+using DAL;
+
+namespace BLL
+{
+    public class ProductBLL
+    {
+        ProductDAL objdal = new ProductDAL();
+
+        // Not sure what we are supposed to do here...
+        #region Read all
+        public List<Product> GetProducts()
+        {
+            try
+            {
+                return objdal;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+        #endregion
+
+        #region Read one
+        public List<Product> GetProdcu
+    }
+}
